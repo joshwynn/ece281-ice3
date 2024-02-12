754151cdaefa6082f8926bb3e1eb2e532c328afd
joshwynn <joshwynn@users.noreply.github.com>
2024-02-12 16:21:22 -0700

    ice 3

--- src/hdl/top_basys3_tb.vhd
+++ src/hdl/top_basys3_tb.vhd
@@ -68,7 +68,7 @@ architecture test_bench of top_basys3_tb is
  
 	-- declare signals needed to stimulate the UUT inputs
 	   -- TODO
-       signal w_sw	:  std_logic_vector(1 downto 0);
+       signal w_sw	:  std_logic_vector(2 downto 0);
        signal w_led :  std_logic_vector(1 downto 0);
 	-- finish declaring needed signals
 begin
